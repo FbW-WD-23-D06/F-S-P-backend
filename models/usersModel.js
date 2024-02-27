d0f5ac5fdc65ed710833f10667af231919aa5a4f
emanuela <emanuela.markovic@hotmail.com>
2024-02-27 14:36:46 +0100

    feature: usersModel.js

---
+++ models/usersModel.js
@@ -0,0 +1,17 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+      minLength: 2,
+      maxLength: 15,
+    },
+  },
+  { timestamps: true, versionKey: false }
+);
+
+const User = model("user", userSchema);
+
+export default User;
