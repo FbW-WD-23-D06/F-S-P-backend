6c6f7f83a4654f0a306c1e0b0824989f2dfce3d2
Jan <j.c.scherch@gmail.com>
2024-03-06 09:31:43 +0100

    adjust postsValidation

--- middleware/postsValidator.js
+++ middleware/postsValidator.js
@@ -2,13 +2,15 @@ import { body } from "express-validator";
 
 const postsValidationRules = [
   body("title")
-    .isLength({ min: 3, max: 100 })
+    .isLength({ min: 3, max: 50 })
     .withMessage(
-      "The title can't be shorter than 3 or longer than 100 characters."
+      "The title can't be shorter than 3 or longer than 50 characters."
     ),
   body("content")
-    .isLength({ min: 10 })
-    .withMessage("The content must to be at least 10 characters long."),
+    .isLength({ min: 10, max: 2000 })
+    .withMessage(
+      "The content must to be at least 10 characters long and not longer than 2000 characters."
+    ),
 ];
 
 export { postsValidationRules };
