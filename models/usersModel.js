096c312a703cfd95a912dd3506c33cd5c61dc9bd
giacomotolari <tolarigiacomo@gmail.com>
2024-03-06 10:04:18 +0100

    feat: password in userschema

--- models/usersModel.js
+++ models/usersModel.js
@@ -9,10 +9,14 @@ const userSchema = new Schema(
       maxLength: 15,
       unique: true,
     },
+    password: {
+      type: String,
+      required: true,
+      minLength: 8,
+    }
   },
   { timestamps: true, versionKey: false }
 );
-
 const User = model("user", userSchema);
 
 export default User;
