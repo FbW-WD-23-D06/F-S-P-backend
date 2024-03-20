d6912aaa61f0569d1076ea18f8d2184a2b6678d0
giacomotolari <tolarigiacomo@gmail.com>
2024-03-20 10:17:37 +0100

    config: cloudinary

---
+++ cloudinary-config.js
@@ -0,0 +1,20 @@
+import cloudinary from "cloudinary";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import multer from "multer";
+
+// Cloudinary configuration
+cloudinary.v2.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+// Multer configuration
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary.v2,
+  params: {
+    folder: "post_paster",
+  },
+});
+
+export const cloudinaryMulter = multer({ storage: storage });
